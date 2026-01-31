1392e0d8b443e3b8b3acf006dae57be146152680
mmmarcin555 <mmmarcin555@gmail.com>
2026-01-31 17:34:20 +0100

    feat: stworzenie interfejsu logowania w WPF i integracja z API

--- ResourceSystem.UI/App.xaml.cs
+++ ResourceSystem.UI/App.xaml.cs
@@ -1,12 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
 using System.Windows;
 
 namespace ResourceSystem.UI;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
 }
