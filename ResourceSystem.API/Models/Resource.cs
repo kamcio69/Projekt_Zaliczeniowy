ba686684cc7c1027f4ffb0717c1b1992856c8afa
mmmarcin555 <mmmarcin555@gmail.com>
2026-01-31 17:14:17 +0100

    feat: implement database models and DbContext

---
+++ ResourceSystem.API/Models/Resource.cs
@@ -0,0 +1,9 @@
+namespace ResourceSystem.API.Models;
+
+public class Resource
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsAvailable { get; set; }
+}
